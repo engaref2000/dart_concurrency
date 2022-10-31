9323f4f1807b056f24556df5c3c4800ea1082000
engaref2000 <engaref2000@gmail.com>
2022-10-31 15:43:58 +0300

    step-4

--- lib/main.dart
+++ lib/main.dart
@@ -38,20 +38,34 @@ extension EmptyOnErrorOnFuture<E> on Future<Iterable<E>> {
   Future<Iterable<E>> emptyOnErr() =>
       catchError((_, __) => Iterable<E>.empty());
 }
+/*
+the Future.forEach function 
+if you don,t care about of result of running the Future , 
+will porcess all the iterable action 
+and feed with null if success 
+and return .catchError((_, __) => -1) here -1 if any error happing 
+
+ */
 
 void testit() async {
-  final persons = await Future.wait([
-    getdate(people1Url).emptyOnErr(),
-    getdate(people2Url).emptyOnErr(),
-  ]);
+  final result = await Future.forEach(
+    Iterable.generate(
+        2, (i) => "http://127.0.0.1:5500/api/people${i + 1}.json"),
+    getdata,
+  ).catchError((_, __) => -1);
+  if (result != null) {
+    'Error accurred'.log();
+  } else {
+    'all Done '.log();
+  }
   // final persons = await getdate(people1Url);
-  persons.log();
+  // persons.log();
 
   ///the resutl is
   ///[(), (Person (foo 2 , 30), Person (bear 2 , 30), Person (baz 2 , 20), Person (koo 2 , 40))]
 }
 
-Future<Iterable<Person>> getdate(String url) => HttpClient()
+Future<Iterable<Person>> getdata(String url) => HttpClient()
     .getUrl(Uri.parse(url))
     .then((req) => req.close())
     .then((resp) => resp.transform(utf8.decoder).join())
